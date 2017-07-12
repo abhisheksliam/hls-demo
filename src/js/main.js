bf38960dc0836f2c6e8aef2073652183581ccc0b
abhisheksliam <abhisheksliam@gmail.com>
2017-07-13 03:13:52 +0530

    ui updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	// Does the browser actually support the video element? 
+	// Does the browser actually support the video element?
 	var supportsVideo = !!document.createElement('video').canPlayType;
 
 	if (supportsVideo) {
@@ -23,18 +23,18 @@
 		// var voldec = document.getElementById('voldec');
 		var progress = document.getElementById('progress');
 		var progressBar = document.getElementById('progress-bar');
-		var fullscreen = document.getElementById('fs');
+		// var fullscreen = document.getElementById('fs');
 
 		// If the browser doesn't support the progress element, set its state for some different styling
 		var supportsProgress = (document.createElement('progress').max !== undefined);
 		if (!supportsProgress) progress.setAttribute('data-state', 'fake');
 
-		// Check if the browser supports the Fullscreen API
-		var fullScreenEnabled = !!(document.fullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitSupportsFullscreen || document.webkitFullscreenEnabled || document.createElement('video').webkitRequestFullScreen);
-		// If the browser doesn't support the Fulscreen API then hide the fullscreen button
-		if (!fullScreenEnabled) {
-			fullscreen.style.display = 'none';
-		}
+		// // Check if the browser supports the Fullscreen API
+		// var fullScreenEnabled = !!(document.fullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitSupportsFullscreen || document.webkitFullscreenEnabled || document.createElement('video').webkitRequestFullScreen);
+		// // If the browser doesn't support the Fulscreen API then hide the fullscreen button
+		// if (!fullScreenEnabled) {
+		// 	fullscreen.style.display = 'none';
+		// }
 
 		// Check the volume
 		var checkVolume = function(dir) {
@@ -59,46 +59,46 @@
 			checkVolume(dir);
 		}
 
-		// Set the video container's fullscreen state
-		var setFullscreenData = function(state) {
-			videoContainer.setAttribute('data-fullscreen', !!state);
-			// Set the fullscreen button's 'data-state' which allows the correct button image to be set via CSS
-			fullscreen.setAttribute('data-state', !!state ? 'cancel-fullscreen' : 'go-fullscreen');
-		}
-
-		// Checks if the document is currently in fullscreen mode
-		var isFullScreen = function() {
-			return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
-		}
-
-		// Fullscreen
-		var handleFullscreen = function() {
-			// If fullscreen mode is active...	
-			if (isFullScreen()) {
-					// ...exit fullscreen mode
-					// (Note: this can only be called on document)
-					if (document.exitFullscreen) document.exitFullscreen();
-					else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
-					else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
-					else if (document.msExitFullscreen) document.msExitFullscreen();
-					setFullscreenData(false);
-				}
-				else {
-					// ...otherwise enter fullscreen mode
-					// (Note: can be called on document, but here the specific element is used as it will also ensure that the element's children, e.g. the custom controls, go fullscreen also)
-					if (videoContainer.requestFullscreen) videoContainer.requestFullscreen();
-					else if (videoContainer.mozRequestFullScreen) videoContainer.mozRequestFullScreen();
-					else if (videoContainer.webkitRequestFullScreen) {
-						// Safari 5.1 only allows proper fullscreen on the video element. This also works fine on other WebKit browsers as the following CSS (set in styles.css) hides the default controls that appear again, and 
-						// ensures that our custom controls are visible:
-						// figure[data-fullscreen=true] video::-webkit-media-controls { display:none !important; }
-						// figure[data-fullscreen=true] .controls { z-index:2147483647; }
-						video.webkitRequestFullScreen();
-					}
-					else if (videoContainer.msRequestFullscreen) videoContainer.msRequestFullscreen();
-					setFullscreenData(true);
-				}
-			}
+		// // Set the video container's fullscreen state
+		// var setFullscreenData = function(state) {
+		// 	videoContainer.setAttribute('data-fullscreen', !!state);
+		// 	// Set the fullscreen button's 'data-state' which allows the correct button image to be set via CSS
+		// 	fullscreen.setAttribute('data-state', !!state ? 'cancel-fullscreen' : 'go-fullscreen');
+		// }
+		//
+		// // Checks if the document is currently in fullscreen mode
+		// var isFullScreen = function() {
+		// 	return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
+		// }
+		//
+		// // Fullscreen
+		// var handleFullscreen = function() {
+		// 	// If fullscreen mode is active...
+		// 	if (isFullScreen()) {
+		// 			// ...exit fullscreen mode
+		// 			// (Note: this can only be called on document)
+		// 			if (document.exitFullscreen) document.exitFullscreen();
+		// 			else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+		// 			else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
+		// 			else if (document.msExitFullscreen) document.msExitFullscreen();
+		// 			setFullscreenData(false);
+		// 		}
+		// 		else {
+		// 			// ...otherwise enter fullscreen mode
+		// 			// (Note: can be called on document, but here the specific element is used as it will also ensure that the element's children, e.g. the custom controls, go fullscreen also)
+		// 			if (videoContainer.requestFullscreen) videoContainer.requestFullscreen();
+		// 			else if (videoContainer.mozRequestFullScreen) videoContainer.mozRequestFullScreen();
+		// 			else if (videoContainer.webkitRequestFullScreen) {
+		// 				// Safari 5.1 only allows proper fullscreen on the video element. This also works fine on other WebKit browsers as the following CSS (set in styles.css) hides the default controls that appear again, and
+		// 				// ensures that our custom controls are visible:
+		// 				// figure[data-fullscreen=true] video::-webkit-media-controls { display:none !important; }
+		// 				// figure[data-fullscreen=true] .controls { z-index:2147483647; }
+		// 				video.webkitRequestFullScreen();
+		// 			}
+		// 			else if (videoContainer.msRequestFullscreen) videoContainer.msRequestFullscreen();
+		// 			setFullscreenData(true);
+		// 		}
+		// 	}
 
 		// Only add the events if addEventListener is supported (IE8 and less don't support it, but that will use Flash anyway)
 		if (document.addEventListener) {
@@ -135,11 +135,11 @@
 				checkVolume();
 			}, false);
 
-			// Add events for all buttons			
+			// Add events for all buttons
 			playpause.addEventListener('click', function(e) {
 				if (video.paused || video.ended) video.play();
 				else video.pause();
-			});			
+			});
 
 			mute.addEventListener('click', function(e) {
 				video.muted = !video.muted;
@@ -151,9 +151,9 @@
 			// voldec.addEventListener('click', function(e) {
 			// 	alterVolume('-');
 			// });
-			fs.addEventListener('click', function(e) {
-				handleFullscreen();
-			});
+			// fs.addEventListener('click', function(e) {
+			// 	handleFullscreen();
+			// });
 
 			// As the video is playing, update the progress bar
 			video.addEventListener('timeupdate', function() {
@@ -171,19 +171,39 @@
 			});
 
 			// Listen for fullscreen change events (from other controls, e.g. right clicking on the video itself)
-			document.addEventListener('fullscreenchange', function(e) {
-				setFullscreenData(!!(document.fullScreen || document.fullscreenElement));
-			});
-			document.addEventListener('webkitfullscreenchange', function() {
-				setFullscreenData(!!document.webkitIsFullScreen);
-			});
-			document.addEventListener('mozfullscreenchange', function() {
-				setFullscreenData(!!document.mozFullScreen);
-			});
-			document.addEventListener('msfullscreenchange', function() {
-				setFullscreenData(!!document.msFullscreenElement);
-			});
+			// document.addEventListener('fullscreenchange', function(e) {
+			// 	setFullscreenData(!!(document.fullScreen || document.fullscreenElement));
+			// });
+			// document.addEventListener('webkitfullscreenchange', function() {
+			// 	setFullscreenData(!!document.webkitIsFullScreen);
+			// });
+			// document.addEventListener('mozfullscreenchange', function() {
+			// 	setFullscreenData(!!document.mozFullScreen);
+			// });
+			// document.addEventListener('msfullscreenchange', function() {
+			// 	setFullscreenData(!!document.msFullscreenElement);
+			// });
 		}
+			// new js code
+
+			var seekEventType = 'input';
+			var seek = document.getElementById('seek');
+
+			addEventHandler(seek, seekEventType, handlerUpdateSeekProgress);
+
+			function handlerUpdateSeekProgress(e) {
+			  var value = e.target.value;
+			  document.getElementById('progress2')
+			    .setAttribute("value", value);
+			}
+
+			function addEventHandler(elem, eventType, handler) {
+			  if (elem.addEventListener)
+			    elem.addEventListener(eventType, handler, false);
+			  else if (elem.attachEvent)
+			    elem.attachEvent('on' + eventType, handler);
+			}
+
 	 }
 
  })();
