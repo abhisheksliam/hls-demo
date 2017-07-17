ffec7a3ed932c5d2c0b4daa3b19ff0a4ffb275df
Abhishek Kumar <abhishek.kumar@comprotechnologies.com>
2017-07-17 15:39:58 +0530

    Initial dist subtree commit

---
+++ dist/js/main.min.js
@@ -0,0 +1,180 @@
+(function () {
+	'use strict';
+
+	// Hide controls in case of no mouse or touch activity for 10 seconds
+	var videoControls = document.getElementById('video-controls'),
+		video = document.getElementById('video'),
+		player = document.getElementById('player');
+
+	// Display controls on mouseover
+	player.addEventListener("mouseover", function () {
+		videoControls.classList.add('display-control');
+	});
+
+	// Hide controls on mouse out of video
+	var isOnPlayer = false;
+
+	player.addEventListener("mouseenter", function () {
+		isOnPlayer = true;
+	});
+
+	// Make mouse out function for nested children
+	function makeMouseOutFn(elem) {
+		var list = traverseChildren(elem);
+		return function onMouseOut(event) {
+			var e = event.toElement || event.relatedTarget;
+			if (!!~list.indexOf(e)) {
+				return;
+			}
+			isOnPlayer = false;
+			hideControls();
+		};
+	}
+
+	player.addEventListener('mouseout', makeMouseOutFn(player), true);
+
+	// Hide controls if no mouse movement for 6 seconds
+	var timer = setTimeout(hideControls, 6000);
+	document.addEventListener("mousemove", function () {
+		if (isOnPlayer) {
+			videoControls.classList.add('display-control');
+		}
+		clearTimeout(timer);
+		timer = setTimeout(hideControls, 6000);
+	});
+
+	function hideControls() {
+		if (!video.paused) {
+			videoControls.classList.remove('display-control');
+		}
+	};
+
+	// Utility function for traverse children nodes
+	function traverseChildren(elem) {
+		var children = [];
+		var q = [];
+		q.push(elem);
+		while (q.length > 0) {
+			var elem = q.pop();
+			children.push(elem);
+			pushAll(elem.children);
+		}
+		function pushAll(elemArray) {
+			for (var i = 0; i < elemArray.length; i++) {
+				q.push(elemArray[i]);
+			}
+		}
+		return children;
+	};
+
+})();
+
+(function () {
+	'use strict';
+
+	var videoSource = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
+
+	var player = document.getElementById('player'),
+		video = document.getElementById('video'),
+		videoControls = document.getElementById('video-controls'),
+		playpause = document.getElementById('playpause'),
+		mute = document.getElementById('mute'),
+		progress = document.getElementById('progress'),
+		progressBar = document.getElementById('progress-bar');
+
+	video.controls = false;
+
+	// HLS video init
+	if (Hls.isSupported()) {
+		var hls = new Hls();
+		hls.loadSource(videoSource);
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED, function () {
+			video.play();
+			initVideoQualityOptions(hls);
+		});
+	}
+
+	// Add events for buttons
+	playpause.addEventListener('click', function (e) {
+		if (video.paused || video.ended) video.play();
+		else video.pause();
+	});
+
+	mute.addEventListener('click', function (e) {
+		video.muted = !video.muted;
+		changeButtonState('mute');
+	});
+
+	// React to the user clicking within the progress bar
+	progress.addEventListener('click', function (e) {
+		var pos = (e.pageX - (this.offsetLeft + this.offsetParent.offsetLeft + player.offsetLeft)) / this.offsetWidth;
+		video.currentTime = pos * video.duration;
+	});
+
+
+	// Wait for the video's meta data to be loaded, then set the progress bar's max value to the duration of the video
+	video.addEventListener('loadedmetadata', function () {
+		progress.setAttribute('max', video.duration);
+	});
+
+	// Add event listeners for video specific events
+	video.addEventListener('play', function () {
+		changeButtonState('playpause');
+	}, false);
+	video.addEventListener('pause', function () {
+		changeButtonState('playpause');
+	}, false);
+
+	// As the video is playing, update the progress bar
+	video.addEventListener('timeupdate', function () {
+		if (!progress.getAttribute('max')) progress.setAttribute('max', video.duration);
+		progress.value = video.currentTime;
+		progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
+	});
+
+	// Changes the button state of certain button's so the correct visuals can be displayed with CSS
+	function changeButtonState(type) {
+		// Play/Pause button
+		if (type == 'playpause') {
+			if (video.paused || video.ended) {
+				playpause.setAttribute('data-state', 'play');
+				videoControls.classList.add('display-control');
+			}
+			else {
+				playpause.setAttribute('data-state', 'pause');
+			}
+		}
+		// Mute button
+		else if (type == 'mute') {
+			mute.setAttribute('data-state', video.muted ? 'unmute' : 'mute');
+		}
+	};
+
+	// Video Quality Controls
+
+	function initVideoQualityOptions(hls) {
+
+		var quality = document.getElementById('quality');
+
+		// update current selection, because quality is auto by default
+		quality.addEventListener('focus', function (event) {
+			this.selectedIndex = hls.currentLevel;
+		});
+
+		// update selection on manual change
+		quality.addEventListener('change', function (event) {
+			var q_level = parseInt(this.value);
+			hls.currentLevel = q_level;
+		});
+
+		//Create and append the options
+		var qualities = hls.levels;
+		for (var i = 0; i < qualities.length; i++) {
+			var option = document.createElement("option");
+			option.value = i;
+			option.text = qualities[i].name + ' p';
+			quality.appendChild(option);
+		}
+	};
+})();
